7ece79c76f6ff9348882623694c4e9a7d92160a2
Minh Pham <minh.pham@3forcom.com>
2020-06-02 20:51:39 +0700

    update news page

--- frontend/src/js/pages.js
+++ frontend/src/js/pages.js
@@ -1,3 +1,4 @@
+/* global Swiper */
 (function($){
     function form_validation(){
         $('.form-validate').each(function(){
@@ -5,7 +6,21 @@
             $this.validate();
         });
     }
+    function slider(){
+        new Swiper('#thumbNews .swiper-container',{
+            slidesPerView:1,
+            pagination: {
+                el: '#thumbNews .swiper-pagination',
+                type: 'bullets',
+            },
+            autoplay:true,
+            simulateTouch:false,
+            mouseDrag:false,
+            touchDrag:false,
+        });
+    }
     $(function(){
         form_validation();
+        slider();
     });
 })(jQuery);

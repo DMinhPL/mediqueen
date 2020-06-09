1168e5bc718d90bb1277b33aa1ac862c0ca5dad9
Minh Pham <minh.pham@3forcom.com>
2020-06-09 22:22:20 +0700

    slicing cart page

--- frontend/src/js/pages.js
+++ frontend/src/js/pages.js
@@ -1,54 +1,53 @@
 /* global Swiper */
-(function($){
-    function form_validation(){
-        $('.form-validate').each(function(){
+(function($) {
+    function form_validation() {
+        $('.form-validate').each(function() {
             const $this = $(this);
             $this.validate();
         });
     }
-    function slider(){
-        new Swiper('#thumbNews .swiper-container',{
-            slidesPerView:1,
+    function slider() {
+        new Swiper('#thumbNews .swiper-container', {
+            slidesPerView: 1,
             pagination: {
                 el: '#thumbNews .swiper-pagination',
                 type: 'bullets',
             },
-            autoplay:true,
-            simulateTouch:false,
-            mouseDrag:false,
-            touchDrag:false,
+            autoplay: true,
+            simulateTouch: false,
+            mouseDrag: false,
+            touchDrag: false,
         });
         // Product thumb slide
-        const thumb = new Swiper('.s__products .thumbDetail .swiper-container',{
-            slidesPerView:'auto',
+        const thumb = new Swiper('.s__products .thumbDetail .swiper-container', {
+            slidesPerView: 'auto',
             spaceBetween: 0,
             pagination: {
                 el: '.s__products .thumbDetail .swiper-pagination',
                 type: 'bullets',
-                clickable:true
+                clickable: true,
             },
             navigation: {
                 nextEl: '.s__products .thumbDetail .swiper-button-next',
                 prevEl: '.s__products .thumbDetail .swiper-button-prev',
-              },
+            },
         });
         const $item = $('.s__products .thumbDetail .swiper-slide');
         const $magnifyThumb = $('.s__products .productItem .magnifyImg .magItem');
-        $item.on('click',function(){
-            const _index  = thumb.clickedIndex;
+        $item.on('click', function() {
+            const _index = thumb.clickedIndex;
             $magnifyThumb.addClass('d-none');
             $('.s__products .productItem .magnifyImg .magItem')
-                .filter(`[data-id=${_index}]`).removeClass('d-none');
-
+                .filter(`[data-id=${_index}]`)
+                .removeClass('d-none');
         });
-
     }
     function droplist_selectbox_handle() {
         const $select = $('.nl-dropbox .nl-select');
         const $item = $('.nl-dropbox .nl-droplist .item-value');
         let result = null;
 
-        $select.on('click', function () {
+        $select.on('click', function() {
             const $this = $(this);
             if (!$this.hasClass('focusing')) {
                 $select.removeClass('focusing');
@@ -58,18 +57,18 @@
 
         let isMouseOutside = false;
         $select
-            .on('mouseenter', function () {
+            .on('mouseenter', function() {
                 isMouseOutside = false;
             })
-            .on('mouseleave', function () {
+            .on('mouseleave', function() {
                 isMouseOutside = true;
             });
 
-        $(window).on('click', function () {
+        $(window).on('click', function() {
             if (isMouseOutside) $select.removeClass('focusing');
         });
 
-        $item.on('click', function (e) {
+        $item.on('click', function(e) {
             result = null;
             const it = e.currentTarget;
             const itVal = $(it).attr('data-value');
@@ -83,21 +82,41 @@
                 .text(result);
         });
     }
-    function scrollToDetail(){
+    function scrollToDetail() {
         const $more = $('.s__products .moreDetail');
         const $detail = $('.s__products .detailPanel.top');
-        $more.on('click',function(e){
+        $more.on('click', function(e) {
             e.preventDefault();
-            $('html,body').animate({
-                scrollTop: $detail.offset().top - 80
-            },800);
-            
+            $('html,body').animate(
+                {
+                    scrollTop: $detail.offset().top - 80,
+                },
+                800
+            );
         });
     }
-    $(function(){
+    function step_payment() {
+        $('#cartProducts').steps({
+            onFinish: function() {
+                alert('Wizard Completed');
+            },
+        });
+
+        const $activePromo = $('.s__cart .card__payment  .btn-custom.activePromo');
+        $activePromo.on('click', function(e) {
+            e.preventDefault();
+            $(this)
+                .closest('.card__payment__content--line')
+                .find('input.inputCode')
+                .removeClass('disabled')
+                .focus();
+        });
+    }
+    $(function() {
         form_validation();
         slider();
         droplist_selectbox_handle();
         scrollToDetail();
+        step_payment();
     });
 })(jQuery);
